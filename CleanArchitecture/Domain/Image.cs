969e6ee401cf073809c096ac5e73e13dbc712e64
engintosun <engintosun@192.168.1.109>
2025-03-16 17:46:12 +0300

    New Model Added

---
+++ CleanArchitecture/Domain/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain;
+
+public class Image
+{
+    public int Id { get; set; }
+    public int PropertyId { get; set; }
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public bool IsUploaded { get; set; }
+    public Property Property { get; set; }
+}
